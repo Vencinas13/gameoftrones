c7b964712a8e08299339d5e972428056ebd369f1
Valentin Encinas <valentin.encinas.rojas@gmail.com>
2024-10-13 16:53:27 +0200

    Add Docker

---
+++ src/main/java/controler/GOTControler.java
@@ -0,0 +1,15 @@
+package controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GOTControler {
+    
+    @GetMapping("/")
+    public String Hello() {
+        return "Hola mundo";
+    }
+    
+}
