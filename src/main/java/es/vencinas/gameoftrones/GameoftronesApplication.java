9bf434111ac5482654108afc09834de12444b3da
Valentin Encinas <valentin.encinas.rojas@gmail.com>
2024-10-13 16:10:10 +0200

    Init

---
+++ src/main/java/es/vencinas/gameoftrones/GameoftronesApplication.java
@@ -0,0 +1,13 @@
+package es.vencinas.gameoftrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameoftronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameoftronesApplication.class, args);
+	}
+
+}
