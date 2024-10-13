477b055ed2dd5760ca38fc79ff78abbeb9b6a3e9
Valentin Encinas <valentin.encinas.rojas@gmail.com>
2024-10-13 17:39:46 +0200

    Move controler

--- src/main/java/es/vencinas/gameoftrones/controler/GOTControler.java
+++ src/main/java/es/vencinas/gameoftrones/controler/GOTControler.java
@@ -1,4 +1,4 @@
-package controler;
+package es.vencinas.gameoftrones.controler;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
