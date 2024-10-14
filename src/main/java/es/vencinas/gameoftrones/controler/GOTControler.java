00777818e310574c1da556c75f69619314b7b7e7
Valentin Encinas <valentin.encinas.rojas@gmail.com>
2024-10-14 12:34:36 +0200

    Modify pom.xml and add service GOTService

--- src/main/java/es/vencinas/gameoftrones/controler/GOTControler.java
+++ src/main/java/es/vencinas/gameoftrones/controler/GOTControler.java
@@ -1,11 +1,16 @@
 package es.vencinas.gameoftrones.controler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.vencinas.gameoftrones.service.GOTService;
 
 @RestController
 public class GOTControler {
+    
+    @Autowired
+    GOTService service;
     
     @GetMapping("/")
     public String Hello() {
---
+++ src/main/java/es/vencinas/gameoftrones/service/GOTService.java
@@ -0,0 +1,9 @@
+package es.vencinas.gameoftrones.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GOTService {
+
+}
+
