ab2a53c5d9555457a7ff9a32239e48014a77e7c2
Valentin Encinas <valentin.encinas.rojas@gmail.com>
2024-10-15 12:47:31 +0200

    Add Zipkin and log spam

--- src/main/java/es/vencinas/gameoftrones/controler/GOTControler.java
+++ src/main/java/es/vencinas/gameoftrones/controler/GOTControler.java
@@ -7,16 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.javafaker.GameOfThrones;
 
 import es.vencinas.gameoftrones.service.GOTService;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
 
 @RestController
 public class GOTControler {
     
     @Autowired
     GOTService service;
+
+    @Autowired
+    Tracer tracer;
     
     @GetMapping("/")
     public String Hello() {
+        Span echoSpan = tracer.nextSpan().name("Prueba").start();
         GameOfThrones got = service.getPersonaje(0);
+        echoSpan.end();
         return got.character().concat(got.city().concat(got.dragon().concat(got.house())));
     }
 }
